627e7113cc65f2deddfecd6ab5bc57025e01b7bd
carlos <carloshabib03@gmail.com>
2019-08-18 13:08:40 -0300

    Updates

---
+++ precache-manifest.fd938bcda796eb54fe904551f98c0bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa659027c5694ce086fcaad807d27008",
+    "url": "/Desafio2B/index.html"
+  },
+  {
+    "revision": "5cadb07bd54ad8af6e1e",
+    "url": "/Desafio2B/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "2f62df30ad2c70a08de0",
+    "url": "/Desafio2B/static/js/2.04be41d7.chunk.js"
+  },
+  {
+    "revision": "5cadb07bd54ad8af6e1e",
+    "url": "/Desafio2B/static/js/main.cb44cf1b.chunk.js"
+  },
+  {
+    "revision": "94067e48ee760e8b2473",
+    "url": "/Desafio2B/static/js/runtime~main.68db1151.js"
+  }
+]);
